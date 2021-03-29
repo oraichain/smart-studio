190dd1b6eec2150a77a9cf26158e3737af1dbf17
tuorai <tubackkhoa@gmail.com>
2021-03-30 03:32:40 +0700

    use new version of monaco

--- bin/bundle-templates.js
+++ bin/bundle-templates.js
@@ -21,7 +21,7 @@
 
 const path = require('path');
 const fs = require('fs');
-const fse = require('fs-extra');
+const shell = require('shelljs');
 
 const templatesDir = process.argv[2];
 const outputPath = process.argv[3] || templatesDir;
@@ -74,9 +74,9 @@ templates.forEach((file) => {
 
 // if different folder then sync it, otherwise just created index.js
 if (outputPath !== templatesDir) {
-  fse.removeSync(path.resolve(outputPath));
-  fse.mkdirpSync(path.resolve(outputPath));
-  fse.copySync(path.resolve(templatesDir), path.resolve(outputPath));
+  shell.rm('-rf', path.resolve(outputPath));
+  shell.mkdir('-p', path.resolve(outputPath));
+  shell.cp('-r', path.resolve(templatesDir), path.resolve(outputPath));
 }
 
 fs.writeFileSync(path.resolve(outputPath, 'index.js'), JSON.stringify(output, null, 2));
