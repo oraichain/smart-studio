f888c532b6b0b5076eb3a3246eeeeb4d1e923220
Pham Tu <tubackkhoa@gmail.com>
2023-01-03 10:01:39 +0700

    add animation toggle folder

--- src/components/TreeNode.tsx
+++ src/components/TreeNode.tsx
@@ -1,37 +1,41 @@
-import React,{FC} from 'react';
+import React, { FC, useState } from 'react';
 
 import { Directory, File, FileType, getIconForFileType, Project } from '../models';
 
-interface Props  { 
+interface Props {
   data: File;
-  onClick?: (file: File) => void; 
-};
+  onClick: (file: File) => void;
+}
 
-const TreeNode: FC<Props> = ({ data, onClick}) => {
-  
-  const children= data.type === FileType.Directory ? (data as Directory).children : null;
-  const isProject = data instanceof Project
-  
+const TreeNode: FC<Props> = ({ data, onClick }) => {
+  const [open, setOpen] = useState(true);
+  const children = data.type === FileType.Directory ? (data as Directory).children : null;
+  const isProject = data instanceof Project;
+  const hasChildren = children && children.length > 0;
   return (
-    
-      <div onClick={()=>{
-        if(data.type !== FileType.Directory) onClick?.(data)
-      }} className={`monaco-tree-row ${children?.length ? 'expanded has-children' : ''}`} style={{ paddingLeft: isProject?0:18,cursor:'pointer' }}>
-       {!isProject && 
-       <div className="content">
-          <div className={`monaco-icon-label ${children?.length ? 'folder_type_model folder-icon' :  getIconForFileType(data.type) + ' file-icon ext-file-icon'}`} title="" style={{ display: 'flex' }}>
-            <div className="monaco-icon-label-description-container">
-              <a className="label-name">{data.name}</a>
-              <span className="label-description" />
-            </div>
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        if (data.type === FileType.Directory) {
+          if (!isProject) return setOpen(!open);
+        }
+        onClick(data);
+      }}
+      className={`monaco-tree-row ${hasChildren ? 'has-children' : ''} ${open ? 'expanded' : 'hidden'}`}
+      style={{ paddingLeft: isProject ? 0 : 18, cursor: 'pointer' }}
+    >
+      {!isProject && (
+        <div className={`monaco-icon-label ${hasChildren ? 'folder-icon' : getIconForFileType(data.type) + ' file-icon ext-file-icon'}`} title="" style={{ display: 'flex' }}>
+          <div className="monaco-icon-label-description-container">
+            <a className="label-name">{data.name}</a>
+            <span className="label-description" />
           </div>
-        </div>}    
+        </div>
+      )}
 
-      {children?.map((file) => (
-        <TreeNode key={file.name} data={file} onClick={onClick} />
-      ))}
+      {children && children.map((file) => <TreeNode key={file.name} data={file} onClick={onClick} />)}
     </div>
   );
-}
+};
 
 export default TreeNode;
